f668db9e85979e4e5a8474bc34a61cfb2781199b
Rob Nagle <happy.ajp050789@gmail.com>
2020-05-17 17:15:33 -0700

    updated find-srt.sh to handle failed downloads more intelligently

--- find-srt.sh
+++ find-srt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ls -lR $1 > raw.temp
+rm missing-subs
+
+ls -lR > raw.temp
 
 awk '
 
@@ -14,20 +16,30 @@ BEGIN { RS = "" } ;
 }
 
 ' raw.temp > folders.temp
-
 rm raw.temp
 
 CURL_VERSION=$
+LANG=en
 
 while read FOLDER; do
   VID_EXT_RE="\.(mpg|MPG|flv|FLV|wmv|WMV|avi|AVI|mkv|MKV|mov|MOV|mp4|MP4)$"
   for FILE in "$FOLDER"/*.*; do
     if [[ "$FILE" =~ $VID_EXT_RE ]]; then
       MD5=$(./md5ex.sh "$FILE")
-      echo $FILE " " $MD5
-      #curl -A "SubDB/1.0 (curl/7.35.0 https://curl.haxx.se)" "http://api.thesubdb.com/?action=download&hash=$MD5&language=en" -o "$FOLDER/engsub.srt"
+    # echo $MD5 " is hash for " $FILE
+      SRT_LOCAL_PATH="$FILE.subtitle.$LANG.srt"
+      curl -A "SubDB/1.0 (curl/7.35.0 https://curl.haxx.se)" "http://api.thesubdb.com/?action=download&hash=$MD5&language=$LANG" -o "$SRT_LOCAL_PATH" 2> curl-result.log
+      if [[ -s "$SRT_LOCAL_PATH" ]] #if the subtitle file created by curl is non-zero, assume it's a real subtitle file 
+        then
+	  echo "OK: Subtitle found for $FILE"
+	else
+	  echo "NULL RESULT: No sub found for $FILE!"
+	  echo "$FILE" >> missing-subs
+	  rm "$SRT_LOCAL_PATH" 
+      fi
     fi
   done
 done < folders.temp
 
 rm folders.temp
+
